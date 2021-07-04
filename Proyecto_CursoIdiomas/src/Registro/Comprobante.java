1481238556404a8a9f49a6ca9e346b7d4e8d7ced
Andrea Cachipuendo <andreavs05@hotmail.com>
2021-07-03 20:22:08 -0500

    Se completo la clase Comprobante

--- Proyecto_CursoIdiomas/src/Registro/Comprobante.java
+++ Proyecto_CursoIdiomas/src/Registro/Comprobante.java
@@ -1,25 +1,45 @@
 package Registro;
 
 import java.util.Date;
+import java.io.File;
+import java.io.FileWriter;
 
 public class Comprobante {
     private int idComprobante;
     private String cliente;
     private Date date;
-    private Idioma idioma;
+    private String idioma;
     private String nivel;
     private double valor;
     private Date fecha_pago;
 
-    public void imprimirComprobante(){
-
+    public String imprimirComprobante(){
+        date = new Date();
+        return "Comprobante No: "+ this.idComprobante+
+                "Cliente: "+this.cliente+
+                "Fecha de emisiòn: "+date.getMonth()+
+                "Idioma: "+this.idioma+
+                "Nivel: "+this.nivel+
+                "Costo: "+calculoPago()+
+                "Fecha de Pago: "+date;
     }
+
     public void guardarComporbante(){
+        String Comprobante = imprimirComprobante();
+        try{
 
+            File datos_Comprobante = new File("C:/Documentos/Datos_Comprobante.txt");
+            FileWriter escribir_datos_Comprobante = new FileWriter(datos_Comprobante,true);
+            escribir_datos_Comprobante.write(Comprobante);
+            escribir_datos_Comprobante.close();
+
+        } catch (Exception e) {
+            System.out.println("Error al escribir");
+        }
     }
 
-    public void calculoPago(){
-
+    public double calculoPago(){
+        return valor*0.12+valor;
     }
 
 
