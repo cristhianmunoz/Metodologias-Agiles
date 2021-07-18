84d94f13c541fbe994263dbcc9438236e32ee251
cristhianmunozepn <cristhian.munoz@epn.edu.ec>
2021-07-18 14:25:02 -0500

    TDD_Students

---
+++ tdd/src/test/java/ec/edu/epn/tdd/courses_Lenguagues/I_List_StudentsTest.java
@@ -0,0 +1,22 @@
+package ec.edu.epn.tdd.courses_Lenguagues;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.*;
+
+public class I_List_StudentsTest {
+
+    @Test
+    public void give_Course_when_finishPeriod_then_ok(){
+        Course curso_1 = new Course("1", "Inglès","Academico 1","4pm-6pm");
+        //During 30 days, this course is active. After its astribute "list" should be eliminate
+        curso_1.getNameFile();
+
+        I_List_Students instance= Mockito.mock(I_List_Students.class);
+        Mockito.when(instance.Eliminar_Lista_Curso(curso_1.getNameFile())).thenReturn(true);
+
+        assertTrue( instance.Eliminar_Lista_Curso(curso_1.getNameFile()));
+    }
+
+}
