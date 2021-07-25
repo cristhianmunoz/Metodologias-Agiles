e8428ffc409a288d8c13fbfbf9ec96d81283584c
Andrea Cachipuendo <andreavs05@hotmail.com>
2021-07-25 05:14:11 -0500

    Pruebas con Mockito

---
+++ Proyecto_CursoIdiomas_Maven/src/main/java/ec/edu/epn/tdd/courses_Languagues/I_BudgetBusiness.java
@@ -0,0 +1,5 @@
+package ec.edu.epn.tdd.courses_Languagues;
+
+public interface I_BudgetBusiness {
+    public double recalculateBudget(double amount);
+}
---
+++ Proyecto_CursoIdiomas_Maven/src/test/java/ec/edu/epn/tdd/courses_Languagues/I_BudgetBusinessTest.java
@@ -0,0 +1,23 @@
+package ec.edu.epn.tdd.courses_Languagues;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.*;
+
+public class I_BudgetBusinessTest {
+
+    @Test
+    public void given_one_double_then_recalculateBudget_ok() {
+        I_BudgetBusiness bb = Mockito.mock(I_BudgetBusiness.class);
+        Mockito.when(bb.recalculateBudget(450.6)).thenReturn(669.6);
+        assertEquals(669.6,bb.recalculateBudget(450.6),1120);
+    }
+
+    @Test
+    public void given_one_double_then_recalculateBudget_error() {
+        I_BudgetBusiness bb = Mockito.mock(I_BudgetBusiness.class);
+        Mockito.when(bb.recalculateBudget(450.6)).thenReturn(669.6);
+        assertEquals(669.6,bb.recalculateBudget(450),-1120);
+    }
+}
